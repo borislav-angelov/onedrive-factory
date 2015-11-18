5f27a6409ae78ec1bb4a52c848b04aaaf323c6cc
Simeon Ivanov <simeon@servmask.com>
2015-11-18 20:49:11 +0200

    The OneDriveClient class has been repaired
    Because of the cloud storage

--- OneDriveClient.php
+++ OneDriveClient.php
@@ -4,73 +4,61 @@ require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'OneDriveCurl.php';
 
 class OneDriveClient
 {
-    const API_URL = "https://api.onedrive.com/v1.0";
+	const API_URL = "https://api.onedrive.com/v1.0";
 
-    protected $accessToken = null;
+	protected $accessToken = null;
 
-    public function __construct($accessToken)
-    {
-        $this->accessToken = $accessToken;
-    }
+	public function __construct($accessToken) {
+		$this->accessToken = $accessToken;
+	}
 
-    public function listDrive() {
-      $api = new OneDriveCurl;
-      $api->setAccessToken($this->accessToken);
-      $api->setBaseURL(self::API_URL);
-      $api->setPath('/drive/root/children');
+	public function listDrive() {
+		$api = new OneDriveCurl;
+		$api->setAccessToken($this->accessToken);
+		$api->setBaseURL(self::API_URL);
+		$api->setPath('/drive/root/children');
 
-      return $api->makeRequest();
-    }
+		return $api->makeRequest();
+	}
 
-    public function listFolder($path) {
-      $api = new OneDriveCurl;
-      $api->setAccessToken($this->accessToken);
-      $api->setBaseURL(self::API_URL);
-      $api->setPath("/drive/root:/$path");
+	public function listFolder($path) {
+		$api = new OneDriveCurl;
+		$api->setAccessToken($this->accessToken);
+		$api->setBaseURL(self::API_URL);
+		$api->setPath("/drive/root:/$path");
 
-      return $api->makeRequest();
-    }
+		return $api->makeRequest();
+	}
 
-    public function createFolder($path) {
-      $api = new OneDriveCurl;
-      $api->setAccessToken($this->accessToken);
-      $api->setBaseURL(self::API_URL);
-      $api->setOption(CURLOPT_POST, true);
-      $api->setPath("/drive/items/root:/new_folder");
-      $api->setOption(CURLOPT_POSTFIELDS, array(
-        'root' => 'root',
-        'path' => $path,
-        ));
+	public function createFolder($pathname) {
+		$api = new OneDriveCurl;
+		$api->setAccessToken($this->accessToken);
+		$api->setBaseURL(self::API_URL);
+		$api->setOption(CURLOPT_PUT, true);
+		$api->setPath("/drive/root:/{$pathname}");
 
-      return $api->makeRequest();
-    }
+	  	return $api->makeRequest();
+	}
 
-    public function uploadFile($path, $filename) {
+	public function uploadFile($inStream, $inSize, $pathname) {
+		$api = new OneDriveCurl;
+		$api->setAccessToken($this->accessToken);
+		$api->setBaseURL(self::API_URL);
+		$api->setOption(CURLOPT_INFILE, $my_file);
+		$api->setOption(CURLOPT_PUT, true);
+		$api->setOption(CURLOPT_INFILESIZE, $inSize);
+		$api->setPath("/drive/root:/{$pathname}:/content");
 
-       $my_file = fopen($filename, 'r');
+		return $api->makeRequest();
+	}
 
-       $api = new OneDriveCurl;
-       $api->setAccessToken($this->accessToken);
-       $api->setBaseURL(self::API_URL);
-       $api->setOption(CURLOPT_CUSTOMREQUEST, 'PUT');
-       $api->setOption(CURLOPT_INFILE, $my_file);
-       $api->setOption(CURLOPT_PUT, true);
-       $api->setOption(CURLOPT_INFILESIZE, filesize($filename));
-       $api->setPath('/drive/root:/$path/$my_file:/content');
+	public function downloadFile($outStream, $pathname) {
+		$api = new OneDriveCurl;
+		$api->setAccessToken($this->accessToken);
+		$api->setBaseURL(self::API_URL);
+		$api->setOption(CURLOPT_FILE, $outStream);
+		$api->setPath("/drive/root:/{$pathname}:/content");
 
-       return $api->makeRequest();
-  }
-
-   public function downloadFile($filename) {
-      $file_path = fopen($filename, "w+");
-      
-      $api = new OneDriveCurl;
-      $api->setAccessToken($this->accessToken);
-      $api->setBaseURL(self::API_URL);
-      $api->setOption(CURLOPT_FILE, $file_path);
-      $api->setPath("/drive/items/$filename/content");
-
-      return $api->makeRequest();      
-   }
+		return $api->makeRequest();      
+	}
 }
-?>
