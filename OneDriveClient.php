530267076b3f27d0e4abdc108caf745746fd611c
Simeon Ivanov <simeon@servmask.com>
2015-11-20 16:44:23 +0200

    The fixed ssl verifypeer

--- OneDriveClient.php
+++ OneDriveClient.php
@@ -1,35 +1,26 @@
 <?php
-
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'OneDriveCurl.php';
-
 class OneDriveClient
 {
 	const API_URL = "https://api.onedrive.com/v1.0";
-
 	protected $accessToken = null;
-
 	public function __construct($accessToken) {
 		$this->accessToken = $accessToken;
 	}
-
 	public function listDrive() {
 		$api = new OneDriveCurl;
 		$api->setAccessToken($this->accessToken);
 		$api->setBaseURL(self::API_URL);
 		$api->setPath('/drive/root/children');
-
 		return $api->makeRequest();
 	}
-
 	public function listFolder($path) {
 		$api = new OneDriveCurl;
 		$api->setAccessToken($this->accessToken);
 		$api->setBaseURL(self::API_URL);
 		$api->setPath("/drive/root:/{$path}");
-
 		return $api->makeRequest();
 	}
-
 	public function createFolder($name) {
 		$api = new OneDriveCurl;
 		$api->setAccessToken($this->accessToken);
@@ -41,10 +32,8 @@ class OneDriveClient
 			'name'   => $name,
 			'folder' => (object) array(),
 		)));
-
 	  	return $api->makeRequest();
 	}
-
 	public function uploadFile($inStream, $inSize, $pathname) {
 		$api = new OneDriveCurl;
 		$api->setAccessToken($this->accessToken);
@@ -53,17 +42,14 @@ class OneDriveClient
 		$api->setOption(CURLOPT_PUT, true);
 		$api->setOption(CURLOPT_INFILESIZE, $inSize);
 		$api->setPath("/drive/root:/{$pathname}:/content");
-
 		return $api->makeRequest();
 	}
-
 	public function downloadFile($outStream, $pathname) {
 		$api = new OneDriveCurl;
 		$api->setAccessToken($this->accessToken);
 		$api->setBaseURL(self::API_URL);
 		$api->setPath("/drive/root:/{$pathname}:/content");
 		$api->setOption(CURLOPT_FILE, $outStream);
-
 		return $api->makeRequest();
 	}
 }
